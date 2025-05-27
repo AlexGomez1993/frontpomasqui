4eed93582efca03ea682444c49a85cee4748b058
Jean Sango <jeanpsv000@gmail.com>
2025-05-27 13:36:13 -0500

    feat: :sparkles: cambio de logo

--- src/components/core/logo.tsx
+++ src/components/core/logo.tsx
@@ -36,7 +36,7 @@ export function Logo({
   return (
     <Box
       component="img"
-      alt="Logo Scala"
+      alt="Logo Pomasqui"
       src={logoUrl}
       sx={{
         alignItems: 'center',
