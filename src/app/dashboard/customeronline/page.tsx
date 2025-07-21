e1e95c4773aeab5dcdd54f68b6ce0fbaa747f5bc
Jean Sango <jeanpsv000@gmail.com>
2025-07-21 18:15:45 -0500

    feat: :sparkles: validacion de facturas en carga de facturas online

--- src/app/dashboard/customeronline/page.tsx
+++ src/app/dashboard/customeronline/page.tsx
@@ -25,6 +25,8 @@ import {
   DialogContentText,
   useTheme,
   Autocomplete,
+  Snackbar,
+  Alert,
 } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 import Pagination from '@mui/material/Pagination';
@@ -207,6 +209,10 @@ const FacturaDialog = ({ open, onClose, onSubmit }: FacturaDialogProps) => {
   const [campanias, setCampanias] = useState<Campaign[]>([]);
   const [formasPago, setFormasPago] = useState<PaymentMethod[]>([]);
   const [locales, setLocales] = useState<Store[]>([]);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMsg, setSnackbarMsg] = useState('');
+  const [snackbarType, setSnackbarType] = useState<'success' | 'error' | 'warning'>('success');
+  const [openDialogValidate, setOpenDialogValidate] = useState(false);
   const [formData, setFormData] = useState<FormData>({
     campania: '',
     local: '',
@@ -310,7 +316,39 @@ const FacturaDialog = ({ open, onClose, onSubmit }: FacturaDialogProps) => {
       }));
     }
   };
+  const handleValidar = async () => {
+    if (!formData.numeroFactura || !formData.local || formData.local == '0') {
+      setSnackbarType('error');
+      setSnackbarMsg('Revise que los campos de local y número de factura esten llenos.');
+      setSnackbarOpen(true);
+      return;
+    }
 
+    try {
+      const response = await axiosClient.post(`/api/facturas/validarFactura`, {
+        numeroFactura: formData.numeroFactura,
+        tienda_id: formData.local
+      });
+
+      if (response.status === 200) {
+        setFormData((prev) => ({ ...prev, numeroFactura: '' }));
+        setOpenDialogValidate(true);
+      }
+    } catch (error: any) {
+      if (error.response && error.response.status === 404) {
+        setSnackbarType('success');
+        setSnackbarMsg('Factura válida. Puedes continuar.');
+        setSnackbarOpen(true);
+      } else {
+        setSnackbarType('error');
+        setSnackbarMsg('Error al validar la factura');
+        setSnackbarOpen(true);
+      }
+    }
+  };
+  const handleCloseDialogValidate = () => {
+    setOpenDialogValidate(false);
+  };
   const handleSubmit = async () => {
     const {
       campania,
@@ -366,6 +404,9 @@ const FacturaDialog = ({ open, onClose, onSubmit }: FacturaDialogProps) => {
     //onClose();
   };
 
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
   return (
     <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
       <DialogTitle>Registro de Nueva Factura</DialogTitle>
@@ -425,6 +466,7 @@ const FacturaDialog = ({ open, onClose, onSubmit }: FacturaDialogProps) => {
               value={formData.numeroFactura}
               onChange={handleChange}
               inputProps={{ maxLength: 6 }}
+              onBlur={handleValidar}
               variant="outlined"
               size="small"
             />
@@ -572,6 +614,42 @@ const FacturaDialog = ({ open, onClose, onSubmit }: FacturaDialogProps) => {
           Registrar
         </Button>
       </DialogActions>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={2000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      >
+        <Alert onClose={handleSnackbarClose} severity={snackbarType} sx={{ width: '100%' }}>
+          {snackbarMsg}
+        </Alert>
+      </Snackbar>
+      <Dialog open={openDialogValidate} onClose={handleCloseDialogValidate} maxWidth="sm" fullWidth>
+        <DialogTitle sx={{ textAlign: 'center', fontSize: '1.25rem', fontWeight: 'bold' }}>
+          Factura Registrada
+        </DialogTitle>
+        <DialogContent sx={{ textAlign: 'center', padding: '16px 24px' }}>
+          <Typography variant="body1" sx={{ fontSize: '1rem', color: '#555', marginBottom: '16px' }}>
+            La factura ya ha sido registrada anteriormente. No se permiten duplicados.
+          </Typography>
+        </DialogContent>
+        <DialogActions sx={{ justifyContent: 'center' }}>
+          <Button
+            onClick={handleCloseDialogValidate}
+            color="secondary"
+            variant="contained"
+            sx={{
+              padding: '8px 16px',
+              backgroundColor: '#f44336',
+              '&:hover': {
+                backgroundColor: '#d32f2f',
+              },
+            }}
+          >
+            Cerrar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Dialog>
   );
 };
